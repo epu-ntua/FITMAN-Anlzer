c9eb0b1f344d94ae5f5f2b8214ee038db068dea4
evmorfia-b <evmorfiabiliri@gmail.com>
2016-01-08 16:56:38 +0200

    add topic only query

--- UIapp/api/esQueryUtils.py
+++ UIapp/api/esQueryUtils.py
@@ -13,12 +13,28 @@ def get_main_facet(topic,account,from_timestamp,until_timestamp):
     main_facet = main_facet %(topic,account,topic,from_timestamp,until_timestamp,account)
     return main_facet
 
+def get_topic_facet(topic,from_timestamp,until_timestamp):
+    main_facet = "\"%s\": { \"date_histogram\": { " \
+                 "\"field\": \"doc.created_at\", \"interval\": \"day\" }," \
+                 " \"global\": true, " \
+                 "\"facet_filter\": { \"fquery\": { \"query\": { \"filtered\": " \
+                 "{ \"query\": " \
+                 "{ \"query_string\": { \"query\": \"doc.entities.hashtags.text:%s\" } }, " \
+                 "\"filter\": " \
+                 "{ \"bool\": { \"must\": [ " \
+                 "{ \"range\": { \"doc.created_at\": { \"from\": %s, \"to\": %s } } }, " \
+                 "{ \"terms\": { \"_type\": [ \"couchbaseDocument\" ] } } " \
+                 " ] } } } } } } },"
+    main_facet = main_facet %(topic,topic,from_timestamp,until_timestamp)
+    return main_facet
+
 
 def build_facet_query(topics,accounts,from_timestamp,until_timestamp):
     query_start = "{\"facets\": {"
     query_end = " },\"size\": 0}"
     query=query_start
     for topic in topics:
+        query+=get_topic_facet(topic,topic,from_timestamp,until_timestamp)
         for account in accounts:
             query+=get_main_facet(topic,account,from_timestamp,until_timestamp)
     query = query[:-1] #just remove last comma
