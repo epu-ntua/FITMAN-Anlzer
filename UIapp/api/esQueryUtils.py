a7205c30b70b13b7f3b9eb4a7a608635fca0b908
evmorfia-b <evmorfiabiliri@gmail.com>
2016-01-20 15:52:51 +0200

    fix

--- UIapp/api/esQueryUtils.py
+++ UIapp/api/esQueryUtils.py
@@ -43,7 +43,7 @@ def build_facet_query(topics,accounts,from_timestamp,until_timestamp):
 
 def build_popular_hashtags_query(size):
     query = "{ \"facets\": { \"terms\": { \"terms\": { \"field\": \"doc.entities.hashtags.text\"," \
-            " \"size\": "+ size + ", \"order\": \"count\" }, " \
+            " \"size\": "+ str(size) + ", \"order\": \"count\" }, " \
             "\"facet_filter\": { \"fquery\": { \"query\": { \"filtered\": " \
             "{ \"query\": { \"bool\": { \"should\": [ { \"query_string\": { \"query\": \"*\" } } ] } }, " \
             "\"filter\": { \"bool\": { \"must\": [ { \"terms\": { \"_type\": [ \"couchbaseDocument\" ] } } ] } } } } } } } }, \"size\": 0 }"
